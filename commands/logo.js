4a1d55c8bd614c2bc3bae40e7a1c1a9c7e799c7b
ippyson <www.ippyson@gmail.com>
2019-04-14 23:32:23 +0200

    changed logo url

--- commands/logo.js
+++ commands/logo.js
@@ -5,7 +5,7 @@ module.exports = {
   group: 'misc',
   usage: 'logo',
   execute(message, args) {
-    const logoURL = 'https://cdn.discordapp.com/icons/468044152970674176/12781169588db1e946b3734cde72101c.webp?';
+    const logoURL = 'https://cdn.discordapp.com/icons/468044152970674176/12781169588db1e946b3734cde72101c.png?';
 
     message.reply('Här kommer serverns nuvarande logotyp',{file: logoURL});
   },
